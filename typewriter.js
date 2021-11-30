d75ad3bf764f8b5942502ea038f14b6dc1c6f68e
Rishard C <rishard.c@hotmail.com>
2021-11-30 14:24:15 -0500

    created typewriter file which takes a sentence and displays it as an animation

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = 'hello there from lighthouse labs';
+let delay = 0;
+let increment = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+  process.stdout.write(char);
+}, delay)
+delay += increment
+}
